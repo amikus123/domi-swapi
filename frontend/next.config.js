2b36a91da391f2719d20d981aed26e74c64a15fe
Amadeusz <amikusek123@gmail.com>
2022-02-14 11:34:28 +0100

    testing vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -11,6 +11,9 @@ const nextConfig = {
   publicRuntime :{
     API_URL:process.env.API_URL,
   },
+  typescript:{
+    ignoreBuildErrors: true,
+  },
   // webpack:config=>{
   //   config.resolve.alias["components"] = path.join(__dirname,"components")
   //   config.resolve.alias["public"] = path.join(__dirname,"public")
